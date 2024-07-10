80971ea0d73c64325996260c0510d3bde8203325
AitakattaSora <momynov.sultan@gmail.com>
2024-07-10 22:39:10 +0500

    fix: wrap createResource into trycatch

--- src/utils/track/createResource.ts
+++ src/utils/track/createResource.ts
@@ -4,27 +4,35 @@ import ffmpegStatic from 'ffmpeg-static';
 import { createAudioResource } from '@discordjs/voice';
 
 export async function createResource(url: string) {
-  const process = ytdl.exec(
-    url,
-    {
-      ffmpegLocation: `"${ffmpegStatic as string}"`,
-      format: 'bestaudio[acodec=opus]/bestaudio',
-      limitRate: '800K',
-      output: '-',
-      quiet: true,
-    },
-    {
-      stdio: ['ignore', 'pipe', 'ignore'],
+  try {
+    const process = ytdl.exec(
+      url,
+      {
+        ffmpegLocation: `"${ffmpegStatic as string}"`,
+        format: 'bestaudio[acodec=opus]/bestaudio',
+        limitRate: '800K',
+        output: '-',
+        quiet: true,
+      },
+      {
+        stdio: ['ignore', 'pipe', 'ignore'],
+      }
+    );
+
+    const stdout = process.stdout;
+    if (!stdout) throw new Error('No stream found');
+
+    const { stream, type } = await demuxProbe(stdout);
+    const resource = createAudioResource(stream, {
+      inputType: type,
+    });
+
+    return resource;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Failed to create resource: ${error.message}`);
     }
-  );
 
-  const stdout = process.stdout;
-  if (!stdout) throw new Error('No stream found');
-
-  const { stream, type } = await demuxProbe(stdout);
-  const resource = createAudioResource(stream, {
-    inputType: type,
-  });
-
-  return resource;
+    throw new Error('Failed to create resource, unknown reason');
+  }
 }
