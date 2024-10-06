c4fde9dc0b16456e8643d1bcf4e964ee4992c5bb
AitakattaSora <momynov.sultan@gmail.com>
2024-10-06 21:52:58 +0500

    Revert "fix: return youtube-sr to get video by id, pass cookie if exists"
    This reverts commit 0817f9ecfbc6c5b86d232f3b5b40b007931c3dfb.

--- src/utils/youtube/getYoutubeTrack.ts
+++ src/utils/youtube/getYoutubeTrack.ts
@@ -1,8 +1,9 @@
 import retry from 'async-retry';
 import getYouTubeID from 'get-youtube-id';
-import ytsr from 'youtube-sr';
 import { Track } from '../../interfaces/Track';
-import { getYtCookiesString } from '../getYtCookiesString';
+import ytsr from 'youtube-sr';
+import yts from 'yt-search';
+import { formatDuration } from '../formatDuration';
 
 export async function getYoutubeTrackByQuery(query: string): Promise<Track> {
   return retry(
@@ -32,19 +33,14 @@ export async function getYoutubeTrackByURL(url: string): Promise<Track> {
     const id = getYouTubeID(url);
     if (!id) throw new Error('Invalid YouTube URL');
 
-    const cookies = await getYtCookiesString();
-    const video = await ytsr.getVideo(url, {
-      headers: {
-        cookie: cookies || '',
-      },
-    });
+    const video = await yts({ videoId: id });
     if (!video) throw new Error('No video found');
 
     return {
       url: video.url,
       title: video.title || 'No title',
-      durationFormatted: video.durationFormatted,
-      durationSec: video.duration,
+      durationFormatted: formatDuration(video.duration.seconds),
+      durationSec: video.duration.seconds,
     };
   } catch (error: any) {
     throw error;
