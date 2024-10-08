894e9f736a0003f33521b9b940757c406a0e2b86
AitakattaSora <momynov.sultan@gmail.com>
2024-10-08 16:38:45 +0500

    fix: use innertube to get youtube video data

---
+++ src/external/youtube/YoutubeClient.ts
@@ -0,0 +1,81 @@
+import getYouTubeID from 'get-youtube-id';
+import { ClientType, Innertube } from 'youtubei.js';
+import { Track } from '../../interfaces/Track';
+import { formatDuration } from '../../utils/formatDuration';
+
+export class YoutubeClient {
+  client: Innertube;
+  private static instance: YoutubeClient | null = null;
+
+  constructor(client: Innertube) {
+    this.client = client;
+  }
+
+  static async create() {
+    if (YoutubeClient.instance) {
+      return YoutubeClient.instance;
+    }
+
+    const client = await Innertube.create({
+      client_type: ClientType.WEB,
+      retrieve_player: false,
+      enable_safety_mode: false,
+      enable_session_cache: true,
+      generate_session_locally: true,
+    });
+
+    YoutubeClient.instance = new YoutubeClient(client);
+    return new YoutubeClient(client);
+  }
+
+  public async byURL(url: string): Promise<Track | null> {
+    try {
+      const id = getYouTubeID(url);
+      if (!id) throw new Error('Invalid YouTube URL');
+
+      const res = await this.client.getBasicInfo(id);
+
+      const duration = res.basic_info.duration;
+      if (typeof duration != 'number' || duration < 0) {
+        throw new Error('Invalid video duration');
+      }
+
+      return {
+        url: 'https://www.youtube.com/watch?v=' + id,
+        title: res.basic_info.title || 'No title',
+        durationSec: duration,
+        durationFormatted: formatDuration(duration),
+      };
+    } catch (error) {
+      console.log('Error byURL', error);
+      throw error;
+    }
+  }
+
+  public async byQuery(query: string): Promise<Track | null> {
+    try {
+      const res = await this.client.search(query, {
+        type: 'video',
+      });
+
+      const vids = (res.videos || []).filter((v) => v.type === 'Video');
+      const video: any = vids[0];
+      if (!video) return null;
+
+      const duration = video?.duration?.seconds;
+      if (typeof duration != 'number' || duration < 0) {
+        throw new Error('Invalid video duration');
+      }
+
+      return {
+        url: `https://www.youtube.com/watch?v=${video.id}`,
+        title: video?.title?.text || 'No title',
+        durationFormatted: formatDuration(duration),
+        durationSec: duration,
+      };
+    } catch (error) {
+      console.log('Error byQuery', error);
+      throw error;
+    }
+  }
+}
--- src/utils/youtube/getYoutubeTrack.ts
+++ src/utils/youtube/getYoutubeTrack.ts
@@ -1,23 +1,17 @@
 import retry from 'async-retry';
-import getYouTubeID from 'get-youtube-id';
+import { YoutubeClient } from '../../external/youtube/YoutubeClient';
 import { Track } from '../../interfaces/Track';
-import ytsr from 'youtube-sr';
-import yts from 'yt-search';
-import { formatDuration } from '../formatDuration';
 
 export async function getYoutubeTrackByQuery(query: string): Promise<Track> {
   return retry(
     async () => {
       try {
-        const video = await ytsr.searchOne(query, 'video', false);
-        if (!video) throw new Error('No video found');
+        const yt = await YoutubeClient.create();
 
-        return {
-          url: video.url,
-          title: video.title || 'No title',
-          durationFormatted: video.durationFormatted,
-          durationSec: video.duration / 1000,
-        };
+        const track = await yt.byQuery(query);
+        if (!track) throw new Error(`No video for ${query} found`);
+
+        return track;
       } catch (error) {
         throw error;
       }
@@ -29,20 +23,21 @@ export async function getYoutubeTrackByQuery(query: string): Promise<Track> {
 }
 
 export async function getYoutubeTrackByURL(url: string): Promise<Track> {
-  try {
-    const id = getYouTubeID(url);
-    if (!id) throw new Error('Invalid YouTube URL');
+  return retry(
+    async () => {
+      try {
+        const yt = await YoutubeClient.create();
 
-    const video = await yts({ videoId: id });
-    if (!video) throw new Error('No video found');
+        const track = await yt.byURL(url);
+        if (!track) throw new Error(`No video for ${url} found`);
 
-    return {
-      url: video.url,
-      title: video.title || 'No title',
-      durationFormatted: formatDuration(video.duration.seconds),
-      durationSec: video.duration.seconds,
-    };
-  } catch (error: any) {
-    throw error;
-  }
+        return track;
+      } catch (error: any) {
+        throw error;
+      }
+    },
+    {
+      retries: 2,
+    }
+  );
 }
